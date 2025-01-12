e623ab16ea0b1defd897571b7ed5b3dae2300f61
rksam <erksg.10.26@gmail.com>
2025-01-12 00:35:52 -0500

    resolviendo responsive

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const url ="https://pokeapi.co/api/v2/pokemon/";
 const divContenedor=document.getElementById("main-contenedor");
 const btnEvent=document.querySelectorAll("button")
 async function fetchPokemonsInOrder() {
-   
-    for( i=1 ; i<=1025; i++){
-        
+
+    for( i=1 ; i<=999; i++){
+
         const response = await fetch(url + i);
         const data = await response.json();
 
         let pokeTipo=data.types.map(obj=> `<p id="${obj.type.name}">${obj.type.name}</p>` );
         pokeTipo=pokeTipo.join(" ");
-    
+
         let numId=data.id;
         const numString=numId.toString();
         if(numString.length===1){
@@ -20,32 +20,34 @@ async function fetchPokemonsInOrder() {
         }
 
             const divPokemon= document.createElement("div");
+            divPokemon.className="container_body";
             const imagenUrl=data.sprites.other["official-artwork"].front_default;
             const gifUrl=data.sprites.other.showdown.front_shiny;
             divPokemon.innerHTML=`
-                <div class="pokemons">
-    
-                <h4 class="poke-numero-id">${numId}</h4>
-                 <div class="poke-imagen">
-                     <img src= ${imagenUrl} alt="pokemon">
-                 </div>
-    
-                 <div class="poke-id">
-                   <p class="id">${"#"+numId}</p>
-                   <h5 class="nombre">${data.forms[0].name}</h5>
-                 </div>
+            <div class="pokemons">
 
-                 <div class="poke-typs">
-                  ${pokeTipo}
-                  </div>
-                 <div class="poke-info">
+                <h4 class="poke-numero-id">${numId}</h4>
+                <div class="poke-imagen">
+                    <img src= ${imagenUrl} alt="pokemon">
+                </div>
+
+                <div class="poke-id">
+                <p class="id">${"#"+numId}</p>
+                <h5 class="nombre">${data.forms[0].name}</h5>
+                </div>
+
+                <div class="poke-typs">
+                ${pokeTipo}
+                </div>
+                <div class="poke-info">
                     <p>${data.height+"CM"}</p>
                     <p>${data.weight+"MG"}</p>
-                 </div>
-              </div>
-              `;
-              divContenedor.appendChild(divPokemon);
-           
+                </div>
+            </div>
+
+            `;
+            divContenedor.appendChild(divPokemon);
+
         }
     }
     fetchPokemonsInOrder();
@@ -53,11 +55,11 @@ async function fetchPokemonsInOrder() {
     btnEvent.forEach(boton => boton.addEventListener("click",(event)=>{
         const botonId=event.currentTarget.id;
         divContenedor.innerHTML=" ";
-        
+
         for(let i=1 ; i<=100; i++){
-         fetch(url+i)
-         .then(response => response.json())
-         .then(data => {
+        fetch(url+i)
+        .then(response => response.json())
+        .then(data => {
                 const listTypes=data.types.map(obj => obj.type.name);
                 if(listTypes.some(tipo => tipo.includes(botonId))){
                     fetchPokemonsInOrder();
